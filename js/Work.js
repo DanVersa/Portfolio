92f64da963b1fcc90d710aa7bae0013716c4812c
Dan Versalov <dan.versalov@gmail.com>
2017-04-13 18:30:53 -0400

    Delete Work.js

--- js/Work.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(window).scroll(function() {
-    if ($(document).scrollTop() > 10) {
-      $("nav").addClass("navcolor");
-    } else {
-      $("nav").removeClass("navcolor");
-    }
-  });
